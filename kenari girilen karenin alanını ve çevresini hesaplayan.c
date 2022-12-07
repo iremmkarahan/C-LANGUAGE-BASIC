7ab1661716c5cd2867ca6b618cce092d4e57dfd5
maryxxx1 <99138600+maryxxx1@users.noreply.github.com>
2022-12-07 20:58:40 +0300

    Add files via upload

---
+++ kenari girilen karenin alanını ve çevresini hesaplayan.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  hello
+//
+//  Created by İrem Karahan on 4.12.2022.
+//
+
+#include <stdio.h>
+main()
+{
+    int sayi,cevre,alan;
+    printf("bir sayi giriniz:");
+    scanf("%d",&sayi);
+    
+    
+    cevre=sayi*4;
+    alan=sayi*sayi;
+    
+    printf("karenin cevresi: %d\n",cevre);
+    printf("karenin alanı: %d\n",alan);
+    
+    
+}
+
